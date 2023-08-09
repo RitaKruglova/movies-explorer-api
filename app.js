d28b686f4b0d7425daa04fdfdcf8e3a12eb5c65c
Rita Kruglova <kruglova@404@mail.ru>
2023-08-09 14:55:31 +0300

    feat: create server

---
+++ app.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const { PORT = 3002, DB_URL = 'mongodb://127.0.0.1:27017/moviesexplorerdb' } = process.env;
+
+const app = express();
+
+mongoose.connect(DB_URL);
+
+app.listen(PORT, () => {
+  console.log('Hello, World');
+});
