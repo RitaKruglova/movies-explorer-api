be2d4ca13c26fcea0eaa0ab2c076c6bc12c90394
Rita Kruglova <kruglova@404@mail.ru>
2023-08-09 16:23:35 +0300

    feat: add movieSchema

---
+++ models/movie.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+
+const movieSchema = mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => {
+        const urlRegex = /https?:\/\/w?w?w?\.?.+\..+/;
+        return urlRegex.test(value);
+      },
+      message: 'Неверный формат URL',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => {
+        const urlRegex = /https?:\/\/w?w?w?\.?.+\..+/;
+        return urlRegex.test(value);
+      },
+      message: 'Неверный формат URL',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => {
+        const urlRegex = /https?:\/\/w?w?w?\.?.+\..+/;
+        return urlRegex.test(value);
+      },
+      message: 'Неверный формат URL',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
