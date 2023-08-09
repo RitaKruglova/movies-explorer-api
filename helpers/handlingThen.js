d35022c3bd88f2b77a9bff8c7b2b05e4b0268883
Rita Kruglova <kruglova@404@mail.ru>
2023-08-09 21:33:37 +0300

    create handleThen for controllers

---
+++ helpers/handlingThen.js
@@ -0,0 +1,10 @@
+module.exports.handleThen = (data, res, statusCode = 200) => {
+  if (data === null) {
+    res.status(404).send({ message: 'Не найдено' });
+  } else {
+    res.set({
+      'Content-Security-Policy': 'default-src "self"',
+    })
+      .status(statusCode).send(data);
+  }
+};
