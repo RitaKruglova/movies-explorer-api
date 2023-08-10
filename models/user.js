375b9593f516fe8e6bb81b2f68cb07ac7d64d19c
Rita Kruglova <kruglova404@mail.ru>
2023-08-10 17:14:14 +0300

    feat: add custom method for user schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
+const { UnauthorizedError } = require('../helpers/errorClasses');
 
 const userSchema = mongoose.Schema({
   email: {
@@ -32,5 +33,23 @@ const userSchema = mongoose.Schema({
     },
   },
 });
+
+userSchema.statics.checkPassword = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError('Вы ввели неправильный логин или пароль.'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new UnauthorizedError('Вы ввели неправильный логин или пароль.'));
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
