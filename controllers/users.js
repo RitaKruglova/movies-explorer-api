7816da42f51230738a460807679054db3da2882b
Rita Kruglova <kruglova404@mail.ru>
2023-08-10 19:00:39 +0300

    feat: add login controller

---
+++ controllers/users.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+const handleThen = require('../helpers/handlingThen');
+
+module.exports.getUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => handleThen(user, res))
+    .catch(next);
+};
+
+module.exports.changeUserInfo = (req, res, next) => {
+  const { email, name } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { email, name }, {
+    new: true,
+    runValidators: true,
+    upsert: false,
+  })
+    .then((user) => handleThen(user, res, 202))
+    .catch(next);
+};
+
+module.exports.createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 20)
+    .then((hash) => {
+      User.create({
+        email: req.body.email,
+        password: hash,
+        name: req.body.name,
+      })
+        .then((user) => {
+          handleThen(user, res, 201);
+        })
+        .catch(next);
+    })
+    .catch(next);
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  User.checkPassword(email, password)
+    .then((user) => {
+      const { SECRET_KEY = 'secret-key', NODE_ENV = 'development' } = process.env;
+
+      const token = jwt.sign(
+        { _id: req.user._id },
+        SECRET_KEY,
+        { expiresIn: '7d' },
+      );
+
+      res.cookie('jwt', token, {
+        httpOnly: false,
+        maxAge: 604800000,
+        secure: NODE_ENV === 'production',
+        SameSite: 'none',
+      });
+
+      res.send(user);
+    })
+    .catch(next);
+};
