5c0f9db3591eacc4af23c44668485de28f22e11c
Rita Kruglova <kruglova@404@mail.ru>
2023-08-11 15:32:29 +0300

    feat: add users routes, signin, signup

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,21 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { getUser, changeUserInfo, createUser, login, logout } = require('../controllers/users');
+const { getUser, changeUserInfo } = require('../controllers/users');
+
+router.get('/me', celebrate({
+  headers: Joi.object().keys({
+    'content-type': Joi.string().valid('application/json').required(),
+  }).unknown(),
+}), getUser);
+
+router.patch('/me', celebrate({
+  headers: Joi.object().keys({
+    'content-type': Joi.string().valid('application/json').required(),
+  }).unknown(),
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2),
+  }),
+}), changeUserInfo);
+
+module.exports = router;
