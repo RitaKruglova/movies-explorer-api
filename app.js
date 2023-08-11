d171890ef2f3656aaa74eaa6f415f1a5eeb619ed
Rita Kruglova <kruglova@404@mail.ru>
2023-08-11 16:09:28 +0300

    feat: add handleCatch middleware

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 const { errors, celebrate, Joi } = require('celebrate');
 const userRouter = require('./routes/users');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { handleCatch } = require('./middlewares/handlingCatch');
 
 const { PORT = 3002, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
@@ -19,6 +21,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(helmet());
+
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+}));
 
 app.post('/signup', celebrate({
   headers: Joi.object().keys({
@@ -46,6 +55,7 @@ app.use(auth);
 app.use('/users', userRouter);
 
 app.use(errors());
+app.use(handleCatch);
 
 app.listen(PORT, () => {
   console.log('Hello, World');
