8e8d95f913b3957a2eb6d680ade81e503f9eff77
Rita Kruglova <kruglova404@mail.ru>
2023-08-14 18:44:22 +0300

    fix: change text of error

--- middlewares/handlingCatch.js
+++ middlewares/handlingCatch.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const { ConflictError, ValidationError, InternalServerError } = require('../helpers/errorClasses');
-const { emailAlreadyUsed, unexpectedError } = require('../constants/errorMessages');
+const { alreadyExist, unexpectedError } = require('../constants/errorMessages');
 
 module.exports.handleCatch = (err, req, res, next) => {
   let verifiedError = err;
 
   if (err.code === 11000) {
-    verifiedError = new ConflictError(emailAlreadyUsed);
+    verifiedError = new ConflictError(alreadyExist);
   }
   if (err instanceof mongoose.Error.ValidationError) {
     verifiedError = new ValidationError(err.message);
