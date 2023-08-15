c7c68db0602b42fadc01b06ce25bc653192db355
Rita Kruglova <kruglova404@mail.ru>
2023-08-15 15:29:35 +0300

    fix: create united route

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const rateLimiter = require('./helpers/rateLimiter');
-const { validateCreateUser, validateLogin } = require('./validation/auth');
+// const { validateCreateUser, validateLogin } = require('./validation/auth');
 const router = require('./routes/index');
-const { createUser, login, logout } = require('./controllers/users');
-const auth = require('./middlewares/auth');
+// const { createUser, login, logout } = require('./controllers/users');
+// const auth = require('./middlewares/auth');
 const { handleCatch } = require('./middlewares/handlingCatch');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { serverIsCrashing } = require('./constants/errorMessages');
+// const { serverIsCrashing } = require('./constants/errorMessages');
 const { checkDomain } = require('./middlewares/cors');
-const { NotFoundError } = require('./helpers/errorClasses');
+// const { NotFoundError } = require('./helpers/errorClasses');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
@@ -33,27 +33,27 @@ app.use(checkDomain);
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error(serverIsCrashing);
-  }, 0);
-});
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error(serverIsCrashing);
+//   }, 0);
+// });
 
-app.post('/signup', validateCreateUser, createUser);
+// app.post('/signup', validateCreateUser, createUser);
 
-app.post('/signin', validateLogin, login);
+// app.post('/signin', validateLogin, login);
 
-app.use(auth);
+// app.use(auth);
 
-// app.use('/users', userRouter);
-// app.use('/movies', movieRouter);
+// // app.use('/users', userRouter);
+// // app.use('/movies', movieRouter);
 app.use('/', router);
 
-app.use('/signout', logout);
+// app.use('/signout', logout);
 
-app.use('*', (req, res, next) => {
-  next(new NotFoundError('Маршрут не найден'));
-});
+// app.use('*', (req, res, next) => {
+//   next(new NotFoundError('Маршрут не найден'));
+// });
 
 app.use(errorLogger);
 app.use(errors());
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { validateChangeUserInfo } = require('../validation/users');
-const { getUser, changeUserInfo } = require('../controllers/users');
+const {
+  getUser,
+  changeUserInfo,
+} = require('../controllers/users');
 
 router.get('/me', getUser);
 
